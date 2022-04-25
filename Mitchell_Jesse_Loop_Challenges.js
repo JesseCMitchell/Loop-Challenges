7879d9fb97c6b392ad1952edc480dc9464da76cc
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-04-25 12:00:00 -0700

    commit

--- Mitchell_Jesse_Loop_Challenges.js
+++ Mitchell_Jesse_Loop_Challenges.js
@@ -1,6 +1,35 @@
-var arr = [];
+// Print odd numbers 1-20
+// var arr = [];
 
-for (var i = 1; i < 20; i += 2) {
-  arr.push(i);
+// for (var i = 1; i < 20; i += 2) {
+//   arr.push(i);
+// }
+// console.log(arr);
+
+// Decreasing mult of 3
+// for (var i = 100; i > -1; i--) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// // Print the sequence
+// for (var i = 4; i > -4; i -= 1.5) {
+//   console.log(i);
+// }
+
+// Sigma
+// var sum = 0;
+
+// for (var i = 1; i <= 100; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// Factorial
+var product = 1;
+
+for (var i = 1; i <= 12; i++) {
+  product *= i;
 }
-console.log(arr);
+console.log(product);
